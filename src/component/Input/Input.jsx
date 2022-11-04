2cab4896cb08b86a15e0c12cfe1f5823ea1b131e
ruviter <turtlesim999@gmail.com>
2022-11-04 09:50:34 +0900

    22222

--- src/component/Input/Input.jsx
+++ src/component/Input/Input.jsx
@@ -5,7 +5,7 @@ const Input = ({addTodo}) => {
   return (
     <>
       <form className={st.form} action="submit" onSubmit={addTodo}>
-        <input className={st.input} type="text" placeholder="Add to do 이슈2드가자~" />
+        <input className={st.input} type="text" placeholder="2222222222" />
         <button className={st.submit} >Add</button>
       </form>
     </>

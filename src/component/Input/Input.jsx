923f06535fe51779b54cde327398758b295f4a28
ruviter <turtlesim999@gmail.com>
2022-11-04 09:51:34 +0900

    33333

--- src/component/Input/Input.jsx
+++ src/component/Input/Input.jsx
@@ -5,7 +5,11 @@ const Input = ({addTodo}) => {
   return (
     <>
       <form className={st.form} action="submit" onSubmit={addTodo}>
+<<<<<<< HEAD
         <input className={st.input} type="text" placeholder="2222222222" />
+=======
+        <input className={st.input} type="text" placeholder="33333333" />
+>>>>>>> 3c8750d (33333)
         <button className={st.submit} >Add</button>
       </form>
     </>
